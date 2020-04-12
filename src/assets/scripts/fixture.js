df4eb21c0f14b90bd8e2a67b4198f821acc1fd3f
mona2711 <49019671+mona2711@users.noreply.github.com>
2020-04-11 19:57:06 -0700

    Update fixture.js

--- src/assets/scripts/fixture.js
+++ src/assets/scripts/fixture.js
@@ -1,13 +1,13 @@
 var fixture_data;
- loadFixtures(8);
+loadFixtures(8);
 
 //function for year selection
 function year_click(clicked_id) {
-  d3.select("svg").remove();
+ d3.select("svg").remove();
  //alert(clicked_id);
  switch (clicked_id) {
   case 'year2019':
-  loadFixtures(8);
+   loadFixtures(8);
    break;
   case 'year2018':
    loadFixtures(7);
@@ -40,57 +40,58 @@ function year_click(clicked_id) {
 
 //function for yearly Fixture 
 function loadFixtures(i) {
- d3.csv("https://raw.githubusercontent.com/jkclai/csc511-project/master/src/assets/csv/Lol_worldChampionship.csv", function(data) {
+ //console.log(i);
+ d3.csv("https://raw.githubusercontent.com/jkclai/csc511-project/master/src/assets/csv/Lol_worldChampionship.csv").then(function(data) {
   //console.log(data[8].Year); 
   fixture_data = {
    "name": data[i].Champion,
-    "img":data[i].Champion_flag,
-	"score":data[i].Score_Champion,
+   "img": data[i].Champion_flag,
+   "score": data[i].Score_Champion,
 
    "children": [{
-   // "name": data[i].Champion,
-	"img":data[i].Champion_flag,
-	"score":data[i].Score_SemiC,
-	"level":"#55AE3A",
-	"Stroke_width":"4px",
+    // "name": data[i].Champion,
+    "img": data[i].Champion_flag,
+    "score": data[i].Score_SemiC,
+    "level": "#55AE3A",
+    "Stroke_width": "4px",
 
     "children": [{
      "name": data[i].Champion,
-	 "img":data[i].Champion_flag,
-	 "score":data[i].Score_SemiC,
-     "level":"#55AE3A",
-	 "Stroke_width":"4px",
+     "img": data[i].Champion_flag,
+     "score": data[i].Score_SemiC,
+     "level": "#55AE3A",
+     "Stroke_width": "4px",
 
 
     }, {
-    "name": data[i].Semifinal_withC,
-	 "score":data[i].Score_Semifinal_withC,
-	 "img":data[i].Semifinal_withC_flag,
-	 "level":"#8da0cb",
-	 "Stroke_width":"3px",
+     "name": data[i].Semifinal_withC,
+     "score": data[i].Score_Semifinal_withC,
+     "img": data[i].Semifinal_withC_flag,
+     "level": "#8da0cb",
+     "Stroke_width": "3px",
 
     }]
 
    }, {
-   // "name": data[i].Runner_Up,
-	"score":data[i].Score_RunnerUp,
-	"img":data[i].RunnerUp_flag,
-    "level":"#8da0cb",
-	"Stroke_width":"3px",
+    // "name": data[i].Runner_Up,
+    "score": data[i].Score_RunnerUp,
+    "img": data[i].RunnerUp_flag,
+    "level": "#8da0cb",
+    "Stroke_width": "3px",
 
     "children": [{
      "name": data[i].Runner_Up,
-	 "score":data[i].Score_SemiR,
-	 "img":data[i].RunnerUp_flag,
-	 "level":"#55AE3A",
-	 "Stroke_width":"4px",
+     "score": data[i].Score_SemiR,
+     "img": data[i].RunnerUp_flag,
+     "level": "#55AE3A",
+     "Stroke_width": "4px",
 
     }, {
      "name": data[i].Semifinal_withR,
-	 "score":data[i].Score_Semifinal_withR,
-	 "img":data[i].Semifinal_withR_flag,
-	 "level":"#8da0cb",
-	 "Stroke_width":"3px",
+     "score": data[i].Score_Semifinal_withR,
+     "img": data[i].Semifinal_withR_flag,
+     "level": "#8da0cb",
+     "Stroke_width": "3px",
 
 
     }]
@@ -102,106 +103,143 @@ function loadFixtures(i) {
  })
 }
 
-function fixture(){
-	var width = 700,
-    height = 500;
+function fixture() {
 
-var tree = d3.layout.tree()
-    .size([height, width -250]);
+ // set the dimensions and margins of the diagram
+ //var margin = {top: 20, right: 90, bottom: 30, left: 90},
+ var width = 700,
+  height = 500;
 
-var diagonal = d3.svg.diagonal()
-    .projection(function (d) {
-        return [d.y, d.x];
-    });
+ // declares a tree layout and assigns the size
+ var treemap = d3.tree()
+  .size([height, width - 250]);
 
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(40,-40)");
+ //  assigns the data to a hierarchy using parent-child relationships
+ var nodes = d3.hierarchy(fixture_data, function(d) {
+  return d.children;
+ });
 
-var root = fixture_data,
-    nodes = tree.nodes(root),
-    links = tree.links(nodes);
+ // maps the node data to the tree layout
+ nodes = treemap(nodes);
 
-var link = svg.selectAll(".link")
-    .data(links)
-    .enter()
-    .append("g")
-    .attr("class", "link");
+ // append the svg object to the body of the page
+ // appends a 'group' element to 'svg'
+ // moves the 'group' element to the top left margin
+ var svg = d3.select("body").append("svg")
+  .attr("width", width)
+  .attr("height", height)
+ g = svg.append("g")
+  .attr("transform", "translate(40,-40)");
 
-link.append("path")
-    .attr("fill", "none")
-    //.style('stroke-width', '3px')
-    //.style("stroke", "#8da0cb")
-	.style("stroke-width", function(d) { return d.target.Stroke_width; })
-    .style("stroke", function(d) { return d.target.level; })
 
-    .attr("d", diagonal);
+ // adds the links between the nodes
 
-link.append("text")
-    .attr("stroke", "red")
-    .style("font", "bold 14px Arial")
-    .attr("transform", function(d) {
-        return "translate(" +
-            ((d.source.y + d.target.y)/2) + "," + 
-            ((d.source.x + d.target.x)/2) + ")";
-    })   
-    .attr("dy", "-0.5em")
-    .attr("text-anchor", "middle")
-    .text(function(d) {
-        console.log(d.target.score);
-         return d.target.score;
-    });
+ var link = g.selectAll(".link")
+  .data(nodes.descendants().slice(1))
+  .enter()
+  .append("g")
+  .attr("class", "link");
 
-var node = svg.selectAll(".node")
-    .data(nodes)
-    .enter()
-    .append("g")
-    .attr("class", "node")
-    .attr("transform", function (d) {
-        return "translate(" + d.y + "," + d.x + ")";
-    });
+ link.append("path")
+  .attr("fill", "none")
+  .style("stroke-width", function(d) {
+   return d.data.Stroke_width;
+  })
+  .style("stroke", function(d) {
+   return d.data.level;
+  })
+  .attr("d", function(d) {
+   return "M" + d.y + "," + d.x +
+    "C" + (d.y + d.parent.y) / 2 + "," + d.x +
+    " " + (d.y + d.parent.y) / 2 + "," + d.parent.x +
+    " " + d.parent.y + "," + d.parent.x;
+  });
 
-node.append("circle")
-    .attr("r", 30)
-    .style("stroke", "#e41a1c");
+ //text to link
+ link.append("text")
+  .attr("stroke", "red")
+  .style("font", "bold 14px Arial")
+  .attr("transform", function(d) {
+   return "translate(" +
+    ((d.y + d.parent.y) / 2) + "," +
+    ((d.x + d.parent.x) / 2) + ")";
+  })
+  .attr("dy", "-0.5em")
+  .attr("text-anchor", "middle")
+  .text(function(d) {
+   //console.log(d.parent.data.score);
+   return d.data.score;
+  });
 
-	node.append("text")
-    .attr("dx", 40)
-    .attr("dy", ".35em")
-    .text(function(d) { return d.name });
-	
-	// Append images
-	 var images = node.append("svg:image")
-	.attr("xlink:href",  function(d) { return d.img;})
-	.attr("x", function(d) { return -25;})
-	.attr("y", function(d) { return -25;})
-	.attr("height", 50)
-	.attr("width", 50);
-  
-   var setEvents = images
-          .on( 'click', function (d) {
-           //show match details
-		   })
+ // adds each node as a group
+ var node = g.selectAll(".node")
+  .data(nodes.descendants())
+  .enter().append("g")
+  .attr("class", function(d) {
+   return "node" +
+    (d.children ? " node--internal" : " node--leaf");
+  })
+  .attr("transform", function(d) {
+   return "translate(" + d.y + "," + d.x + ")";
+  });
 
-          .on( 'mouseenter', function() {
-            // select element in current context
-            d3.select( this )
-              .transition()
-              .attr("x", function(d) { return -35;})
-              .attr("y", function(d) { return -35;})
-              .attr("height", 80)
-              .attr("width", 80);
-          })
-          // set back
-          .on( 'mouseleave', function() {
-            d3.select( this )
-              .transition()
-              .attr("x", function(d) { return -25;})
-              .attr("y", function(d) { return -25;})
-              .attr("height", 50)
-              .attr("width", 50);
-          });
-	
+ //add node circle 
+ node.append("circle")
+  .attr("r", 30)
+  .style("stroke", "#e41a1c");
+
+ //add text to the node
+ node.append("text")
+  .attr("dx", 40)
+  .attr("dy", ".35em")
+  .text(function(d) {
+   return d.data.name;
+  });
+
+ // Append images
+ var images = node.append("svg:image")
+  .attr("xlink:href", function(d) {
+   return d.data.img;
+  })
+  .attr("x", function(d) {
+   return -25;
+  })
+  .attr("y", function(d) {
+   return -25;
+  })
+  .attr("height", 50)
+  .attr("width", 50);
+
+ var setEvents = images
+  .on('click', function(d) {
+   //show match details
+  })
+
+  .on('mouseenter', function() {
+   // select element in current context
+   d3.select(this)
+    .transition()
+    .attr("x", function(d) {
+     return -35;
+    })
+    .attr("y", function(d) {
+     return -35;
+    })
+    .attr("height", 80)
+    .attr("width", 80);
+  })
+  // set back
+  .on('mouseleave', function() {
+   d3.select(this)
+    .transition()
+    .attr("x", function(d) {
+     return -25;
+    })
+    .attr("y", function(d) {
+     return -25;
+    })
+    .attr("height", 50)
+    .attr("width", 50);
+  });
+
 }
